65a885dff23b1941cce76ca541c1873acc6f6bc8
CesarConopoima <cesar.conopoima@gmail.com>
2014-11-03 22:14:50 -0430

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fitgenetic::Application.config.secret_token = '5dee001d07217f1d20cebed5419e17c9ef15897bb80ca4139fa2feb7e40a93325a41db8682cc69899cd04ba8495c5ceaeb5eba508ef099766f20ec27da78ce44'
